0973d4824e4ad2cfe722f180847eadbfe197c91e
Yeldon <88385002+Yeldon@users.noreply.github.com>
2022-09-23 16:56:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)
 
 
 # optionals
-auth = os.environ.get("AUTH", "1934595699")
+auth = os.environ.get("AUTH", "5072965573")
 ban = os.environ.get("BAN", "")
 
 
